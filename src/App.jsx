93aa5895775c9cc95e570ef33beeca3673692ba9
Bunyameen <bunyameen@gmail.com>
2023-02-21 19:48:56 +0100

    feat: WIP

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
 import { useState } from 'react'
+import {BrowserRouter as Router} from 'react-router-dom'
+import Footer from './components/Layouts/Footer'
+import Navbar from './components/Layouts/Navbar'
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App bg-teal-500">
-      <h1 className='text-4xl'>Hello World</h1>
-      <button className="btn">Click</button>
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+        <main>
+          <div className="container px-3 pb-12 mx-auto">
+            Content
+          </div>
+        </main>
+        <Footer />
 
-    </div>
+      </div>
+      
+      
+     
+
+    </Router>
   )
 }
 
